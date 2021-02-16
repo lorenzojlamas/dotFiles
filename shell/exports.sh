68e682f173794f1d1130947f28900fb3cf325545
Lorenzo Lamas <lorenzojlamas@gmail.com>
2021-02-16 13:30:00 -0300

    feat: add nvm exports

--- shell/exports.sh
+++ shell/exports.sh
@@ -6,7 +6,9 @@ export FZF_DEFAULT_OPTS='
   --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
   --reverse
 '
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 export path=(
   "$HOME/bin"
   "$DOTLY_PATH/bin"
